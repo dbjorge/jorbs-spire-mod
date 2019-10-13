b6b6093cc2e71112981992feae7c520b55c35bc7
Dan Bjorge <dan@dbjorge.net>
2019-10-13 03:18:25 -0700

    add placeholder comments for remaining memories in RememberRandomNewMemoryAction

--- src/main/java/stsjorbsmod/actions/RememberRandomNewMemoryAction.java
+++ src/main/java/stsjorbsmod/actions/RememberRandomNewMemoryAction.java
@@ -19,13 +19,21 @@ public class RememberRandomNewMemoryAction extends AbstractGameAction  {
 
     public void update() {
         ArrayList<AbstractMemoryPower> candidates = new ArrayList<>();
+
+        // candidates.add(new CharityMemoryPower(target, source, isClarified));
         candidates.add(new ChastityMemoryPower(target, source, isClarified));
         candidates.add(new DiligenceMemoryPower(target, source, isClarified));
+        // candidates.add(new EnvyMemoryPower(target, source, isClarified));
+        // candidates.add(new GreedMemoryPower(target, source, isClarified));
+        // candidates.add(new GluttonyMemoryPower(target, source, isClarified));
         candidates.add(new HumilityMemoryPower(target, source, isClarified));
         candidates.add(new KindnessMemoryPower(target, source, isClarified));
         candidates.add(new LustMemoryPower(target, source, isClarified));
         candidates.add(new PatienceMemoryPower(target, source, isClarified));
+        // candidates.add(new PrideMemoryPower(target, source, isClarified));
         candidates.add(new SlothMemoryPower(target, source, isClarified));
+        // candidates.add(new TemperanceMemoryPower(target, source, isClarified));
+        // candidates.add(new WrathMemoryPower(target, source, isClarified));
 
         candidates.removeIf(memory -> target.hasPower(memory.ID));
 
