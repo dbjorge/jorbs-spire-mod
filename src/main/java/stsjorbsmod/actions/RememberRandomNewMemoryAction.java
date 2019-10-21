c1828e616d5d8d9c464473b7d93f121a886a5a6a
Dan Bjorge <dan@dbjorge.net>
2019-10-20 20:11:07 -0700

    Fix Eye of the Storm clarity detection

--- src/main/java/stsjorbsmod/actions/RememberRandomNewMemoryAction.java
+++ src/main/java/stsjorbsmod/actions/RememberRandomNewMemoryAction.java
@@ -19,8 +19,7 @@ public class RememberRandomNewMemoryAction extends AbstractGameAction  {
 
     public void update() {
         ArrayList<AbstractMemory> candidates = MemoryUtils.allPossibleMemories(target, isClarified);
-
-        candidates.removeIf(memory -> target.hasPower(memory.ID));
+        candidates.removeIf(memory -> MemoryManager.forPlayer(target).hasClarity(memory.ID));
 
         if (!candidates.isEmpty()) {
             int randomIndex = AbstractDungeon.cardRandomRng.random(0, candidates.size() - 1);
