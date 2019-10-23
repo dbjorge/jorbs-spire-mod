4585f757e873227dafd58f5bc258384cc51f3688
Frenoss <Frenoss@users.noreply.github.com>
2019-10-22 22:12:14 -0700

    Manipulation rare: Ivory Tower (#25)
    * Manipulation rare: Ivory Tower
    
    * Generalize the exhaust action.
    
    * Follow pattern for non-instant action.

---
+++ src/main/java/stsjorbsmod/actions/ExhaustCardsMatchingPredicateAction.java
@@ -0,0 +1,50 @@
+package stsjorbsmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+// Based on ExhaustSpecificCardAction
+public class ExhaustCardsMatchingPredicateAction extends AbstractGameAction {
+    private float startingDuration;
+    private CardGroup pile;
+    private Predicate<AbstractCard> filter;
+
+    public ExhaustCardsMatchingPredicateAction(
+            AbstractCreature source, CardGroup pile, Predicate<AbstractCard> filter)
+    {
+        this.setValues(AbstractDungeon.player, source, 1);
+        this.actionType = ActionType.EXHAUST;
+        this.startingDuration = Settings.ACTION_DUR_FAST;
+        this.duration = this.startingDuration;
+        this.pile = pile;
+        this.filter = filter;
+    }
+
+    @Override
+    public void update() {
+        if (startingDuration == duration) {
+            ArrayList<AbstractCard> toExhaust = new ArrayList<AbstractCard>();
+            for (AbstractCard c : pile.group) {
+                if (filter.test(c)) {
+                    toExhaust.add(c);
+                }
+            }
+
+            for (AbstractCard c : toExhaust) {
+                pile.moveToExhaustPile(c);
+                CardCrawlGame.dungeon.checkForPactAchievement();
+                c.exhaustOnUseOnce = false;
+                c.freeToPlayOnce = false;
+            }
+        }
+
+        tickDuration();
+    }
+}
