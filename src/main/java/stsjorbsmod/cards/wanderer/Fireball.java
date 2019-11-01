9edf528871acad19f3c6db3ac1083d95c9471e9b
James Wang <james.wang429@gmail.com>
2019-10-31 20:27:36 -0400

    Make Fireball more inline with Jorbs' spreadsheet

--- src/main/java/stsjorbsmod/cards/wanderer/Fireball.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Fireball.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
@@ -12,9 +13,16 @@ import stsjorbsmod.characters.Wanderer;
 import stsjorbsmod.memories.LustMemory;
 import stsjorbsmod.powers.BurningPower;
 
+import java.util.Iterator;
+
 import static stsjorbsmod.JorbsMod.makeCardPath;
 import static stsjorbsmod.characters.Wanderer.Enums.REMEMBER_MEMORY;
 
+/**
+ * Base: Deal 21 damage to all enemies
+ * Remember Lust
+ * Upgraded: Apply 3 Burning to all enemies
+ */
 public class Fireball extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(Fireball.class.getSimpleName());
     public static final String IMG = makeCardPath("Damage_Rares/fireball.png");
@@ -26,8 +34,7 @@ public class Fireball extends CustomJorbsModCard {
 
     private static final int COST = 2;
     private static final int DAMAGE = 21;
-    private static final int UPGRADE_PLUS_DMG = 7;
-    private static final int UPGRADE_PLUS_BURNING = 3;
+    private static final int BURNING = 3;
 
     public Fireball() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -41,8 +48,10 @@ public class Fireball extends CustomJorbsModCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToTop(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AttackEffect.FIRE));
         addToBot(new RememberSpecificMemoryAction(new LustMemory(p, false)));
-        if(upgraded) {
-            addToBot(new ApplyPowerAction(m, p, new BurningPower(m, p, magicNumber)));
+        if(upgraded && magicNumber > 0) {
+            for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                addToBot(new ApplyPowerAction(mo, p, new BurningPower(mo, p, this.magicNumber)));
+            }
         }
     }
 
@@ -50,8 +59,7 @@ public class Fireball extends CustomJorbsModCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_BURNING);
+            upgradeMagicNumber(BURNING);
             upgradeDescription();
         }
     }
