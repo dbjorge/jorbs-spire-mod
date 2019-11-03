ee701b1e8cd212b54e981503f02d60f0fe801e42
Dan Bjorge <dan@dbjorge.net>
2019-11-02 23:35:24 -0700

    Update numbers on Mindworm upgrade

--- src/main/java/stsjorbsmod/cards/wanderer/Mindworm.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Mindworm.java
@@ -25,8 +25,9 @@ public class Mindworm extends CustomJorbsModCard {
 
     private static final int COST = 0;
     private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DAMAGE = 1;
     private static final int BONUS_SNAPPED_DAMAGE = 8;
-    private static final int UPGRADE_BONUS_SNAPPED_DAMAGE = 8;
+    private static final int UPGRADE_PLUS_BONUS_SNAPPED_DAMAGE = 7;
 
     public Mindworm() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -49,7 +50,8 @@ public class Mindworm extends CustomJorbsModCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_BONUS_SNAPPED_DAMAGE);
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            upgradeMagicNumber(UPGRADE_PLUS_BONUS_SNAPPED_DAMAGE);
             initializeDescription();
         }
     }
