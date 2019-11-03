4a4838ed8a41c8739393a2157b074189dae4e3eb
James Wang <james.wang429@gmail.com>
2019-11-03 14:53:00 -0500

    make amnesia use new action to only remove buffs and debuffs

--- src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
@@ -3,13 +3,18 @@ package stsjorbsmod.cards.wanderer;
 import com.megacrit.cardcrawl.actions.unique.RemoveAllPowersAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.RemovePowersMatchingPredicateAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.actions.SnapAction;
 import stsjorbsmod.characters.Wanderer;
 
 import static stsjorbsmod.JorbsMod.makeCardPath;
 
+/**
+ * TODO move to curse
+ */
 public class Amnesia extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(Amnesia.class.getSimpleName());
     public static final String IMG = makeCardPath("Bad_Rares/amnesia.png");
@@ -29,7 +34,7 @@ public class Amnesia extends CustomJorbsModCard {
     public void use(AbstractPlayer p, AbstractMonster _) {
         // Order matters here, since Clarities are technically powers and we want Snap to consider them.
         addToBot(new SnapAction(p));
-        addToBot(new RemoveAllPowersAction(p, upgraded));
+        addToBot(new RemovePowersMatchingPredicateAction(p, power -> power.type == AbstractPower.PowerType.BUFF || power.type == AbstractPower.PowerType.DEBUFF));
     }
 
     @Override
