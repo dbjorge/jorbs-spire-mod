ee57c7fece07dd432900db38581a251772c6b296
James Wang <james.wang429@gmail.com>
2019-11-03 20:25:13 -0500

    SnappedPower should be unblockable uncleansable (#96)
    * create special enum
    
    * create RemovePowersMatchingPredicateAction
    
    * make amnesia use new action to only remove buffs and debuffs
    
    * make Amnesia an unplayable curse and perform action at the end of turn
    
    * Re-order Amnesia text ordering to better match reality.
    
    * change a comment

--- src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
@@ -1,25 +1,36 @@
 package stsjorbsmod.cards.wanderer;
 
 import com.megacrit.cardcrawl.actions.unique.RemoveAllPowersAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.RemovePowersMatchingPredicateAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.actions.SnapAction;
 import stsjorbsmod.characters.Wanderer;
 
 import static stsjorbsmod.JorbsMod.makeCardPath;
 
+/**
+ * Curse
+ * - Unplayable
+ * - At the end of player turn, Snap.
+ * - Remove player buffs and debuffs.
+ */
 public class Amnesia extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(Amnesia.class.getSimpleName());
     public static final String IMG = makeCardPath("Bad_Rares/amnesia.png");
 
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.SKILL;
-    public static final CardColor COLOR = Wanderer.Enums.WANDERER_GRAY_COLOR;
+    private static final CardType TYPE = CardType.CURSE;
+    public static final CardColor COLOR = CardColor.CURSE;
 
-    private static final int COST = 0;
+    private static final int COST = -2;
 
     public Amnesia() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -28,15 +39,24 @@ public class Amnesia extends CustomJorbsModCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster _) {
         // Order matters here, since Clarities are technically powers and we want Snap to consider them.
-        addToBot(new SnapAction(p));
-        addToBot(new RemoveAllPowersAction(p, upgraded));
+        if(dontTriggerOnUseCard) {
+            addToBot(new SnapAction(p));
+            addToBot(new RemovePowersMatchingPredicateAction(p, power -> power.type == AbstractPower.PowerType.BUFF || power.type == AbstractPower.PowerType.DEBUFF));
+        }
+    }
+
+    @Override
+    public void triggerOnEndOfTurnForPlayingCard() {
+        dontTriggerOnUseCard = true;
+        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(this, true));
     }
 
     @Override
     public void upgrade() {
-        if (!upgraded) {
-            upgradeName();
-            upgradeDescription();
-        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Amnesia();
     }
 }
