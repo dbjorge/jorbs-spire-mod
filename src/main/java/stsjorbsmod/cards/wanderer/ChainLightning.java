8cc3eae9306fdbd926b9a6759bdc97f058cdce79
Steve Matney <steve.matney@gmail.com>
2019-11-04 21:38:52 -0700

    Adding lightning effect to ChainLightning.

--- src/main/java/stsjorbsmod/cards/wanderer/ChainLightning.java
+++ src/main/java/stsjorbsmod/cards/wanderer/ChainLightning.java
@@ -1,15 +1,20 @@
 package stsjorbsmod.cards.wanderer;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.actions.ChainLightningAction;
 import stsjorbsmod.characters.Wanderer;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import static stsjorbsmod.JorbsMod.makeCardPath;
 
@@ -36,7 +41,23 @@ public class ChainLightning extends CustomJorbsModCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         ArrayList<AbstractMonster> targets = AbstractDungeon.getMonsters().monsters;
-        addToBot(new ChainLightningAction(p, m, targets, damage, magicNumber, AttackEffect.SLASH_VERTICAL));
+        addLightningEffect(targets.iterator());
+        addToBot(new ChainLightningAction(p, m, targets, damage, magicNumber, AttackEffect.NONE));
+    }
+
+    private void addLightningEffect(Iterator monsterIterator) {
+        AbstractMonster monster;
+        float duration = 0F;
+        int index = 0;
+        while(monsterIterator.hasNext()) {
+            monster = (AbstractMonster)monsterIterator.next();
+            if (!monster.isDeadOrEscaped()) {
+                duration += (0.1F * index);
+                index += 1;
+                AbstractDungeon.actionManager.addToBottom(new SFXAction("THUNDERCLAP", duration));
+                AbstractDungeon.actionManager.addToBottom(new VFXAction(new LightningEffect(monster.drawX, monster.drawY), duration));
+            }
+        }
     }
 
     @Override
