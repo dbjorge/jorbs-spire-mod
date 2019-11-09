0ae0e716369629a652911802ffe4690e0868a00c
Dan Bjorge <dan@dbjorge.net>
2019-11-09 14:58:26 -0800

    Corpse Explosion: rarity updated to rare, reordered remember/damage, ethereal

--- src/main/java/stsjorbsmod/cards/wanderer/CorpseExplosion_Wanderer.java
+++ src/main/java/stsjorbsmod/cards/wanderer/CorpseExplosion_Wanderer.java
@@ -17,15 +17,15 @@ import static stsjorbsmod.characters.Wanderer.Enums.REMEMBER_MEMORY;
 
 public class CorpseExplosion_Wanderer extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(CorpseExplosion_Wanderer.class.getSimpleName());
-    public static final String IMG = makeCardPath("Damage_Uncommons/corpse_explosion_wanderer.png");
+    public static final String IMG = makeCardPath("Damage_Rares/corpse_explosion_wanderer.png");
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Wanderer.Enums.WANDERER_GRAY_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 12;
+    private static final int DAMAGE = 14;
     private static final int UPGRADE_PLUS_DAMAGE = 4;
 
     public CorpseExplosion_Wanderer() {
@@ -33,14 +33,15 @@ public class CorpseExplosion_Wanderer extends CustomJorbsModCard {
         baseDamage = DAMAGE;
         EntombedField.entombed.set(this, true);
         exhaust = true;
+        isEthereal = true;
 
         tags.add(REMEMBER_MEMORY);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new RememberSpecificMemoryAction(p, WrathMemory.STATIC.ID));
         addToBot(new DamageAction(m, new DamageInfo(p, damage), AttackEffect.FIRE));
+        addToBot(new RememberSpecificMemoryAction(p, WrathMemory.STATIC.ID));
     }
 
     @Override
