afaa19f203e8a190cd9d44fabd2ab0f17e6ce358
Dan Bjorge <dan@dbjorge.net>
2019-11-09 22:10:34 -0800

    Oak Leaf: added

---
+++ src/main/java/stsjorbsmod/cards/wanderer/OakLeaf.java
@@ -0,0 +1,43 @@
+package stsjorbsmod.cards.wanderer;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Wanderer;
+
+import static stsjorbsmod.JorbsMod.makeCardPath;
+
+public class OakLeaf extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(OakLeaf.class.getSimpleName());
+    public static final String IMG = makeCardPath("Material_Components/oak_leaf.png");
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Wanderer.Enums.WANDERER_GRAY_COLOR;
+
+    private static final int COST = 0;
+    private static final int DEXTERITY = 1;
+    private static final int UPGRADE_PLUS_DEXTERITY = 1;
+
+    public OakLeaf() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DEXTERITY;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_DEXTERITY);
+        }
+    }
+}
