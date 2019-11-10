c6a5a1df12356a5c41d5b06e8908da5af37b455c
mderb4 <mderb4@gmail.com>
2019-11-10 00:05:19 -0500

    Reorder memories and add Sulfur and Web. (#128)

--- src/main/java/stsjorbsmod/memories/MemoryUtils.java
+++ src/main/java/stsjorbsmod/memories/MemoryUtils.java
@@ -17,21 +17,21 @@ public class MemoryUtils {
     public static ArrayList<AbstractMemory> allPossibleMemories(AbstractCreature target) {
         ArrayList<AbstractMemory> powers = new ArrayList<>();
 
-        powers.add(new TemperanceMemory(target));
         powers.add(new PatienceMemory(target));
-        powers.add(new KindnessMemory(target));
-        powers.add(new HumilityMemory(target));
         powers.add(new DiligenceMemory(target));
         powers.add(new ChastityMemory(target));
+        powers.add(new KindnessMemory(target));
+        powers.add(new HumilityMemory(target));
         powers.add(new CharityMemory(target));
+        powers.add(new TemperanceMemory(target));
 
+        powers.add(new LustMemory(target));
         powers.add(new EnvyMemory(target));
+        powers.add(new WrathMemory(target));
         powers.add(new GreedMemory(target));
         powers.add(new GluttonyMemory(target));
-        powers.add(new LustMemory(target));
         powers.add(new PrideMemory(target));
         powers.add(new SlothMemory(target));
-        powers.add(new WrathMemory(target));
 
         return powers;
     }
