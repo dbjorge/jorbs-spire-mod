0f26716437dc552a17fd7359309e4123036123e8
Dan Bjorge <dan@dbjorge.net>
2019-11-11 00:09:24 -0800

    Add Energy Bulb and Loose Leaf

---
+++ src/main/java/stsjorbsmod/cards/wanderer/materialcomponents/LooseLeaf.java
@@ -0,0 +1,46 @@
+package stsjorbsmod.cards.wanderer.materialcomponents;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Wanderer;
+import stsjorbsmod.patches.EphemeralField;
+
+import static stsjorbsmod.JorbsMod.makeCardPath;
+
+public class LooseLeaf extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(LooseLeaf.class.getSimpleName());
+    public static final String IMG = makeCardPath("Material_Components/loose_leaf.png");
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Wanderer.Enums.WANDERER_GRAY_COLOR;
+
+    private static final int COST = 0;
+    private static final int DRAW = 1;
+    private static final int UPGRADE_PLUS_DRAW = 1;
+
+    public LooseLeaf() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DRAW;
+        EphemeralField.ephemeral.set(this, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DrawCardAction(p, magicNumber));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_DRAW);
+            upgradeDescription();
+        }
+    }
+}
