531037c76d3e25ff3f3613e42f4191a3ee90856b
Dan Bjorge <dan@dbjorge.net>
2019-11-10 22:23:05 -0800

    Gather Power, Quicksilver: rework upgrade effects to apply more attack to the same card

--- src/main/java/stsjorbsmod/cards/wanderer/materialcomponents/Quicksilver.java
+++ src/main/java/stsjorbsmod/cards/wanderer/materialcomponents/Quicksilver.java
@@ -8,6 +8,7 @@ import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.actions.AdvanceRelicsThroughTimeAction;
 import stsjorbsmod.characters.Wanderer;
+import stsjorbsmod.powers.PlayNextAttackThisTurnAdditionalTimesPower;
 
 import static stsjorbsmod.JorbsMod.makeCardPath;
 
@@ -22,27 +23,27 @@ public class Quicksilver extends CustomJorbsModCard {
 
     private static final int COST = 1;
     private static final int RELIC_COUNTER_INCREMENT = 1;
-    private static final int DOUBLE_TAP_TURNS = 1;
-    private static final int UPGRADE_PLUS_DOUBLE_TAP_TURNS = 1;
+    private static final int NEXT_ATTACK_ADDITIONAL_TIMES = 1;
+    private static final int UPGRADE_PLUS_NEXT_ATTACK_ADDITIONAL_TIMES = 1;
 
     public Quicksilver() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = DOUBLE_TAP_TURNS;
+        magicNumber = baseMagicNumber = NEXT_ATTACK_ADDITIONAL_TIMES;
         exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new AdvanceRelicsThroughTimeAction(p, RELIC_COUNTER_INCREMENT));
-        addToBot(new ApplyPowerAction(p, p, new DoubleTapPower(p, magicNumber), magicNumber));
+        addToBot(new ApplyPowerAction(p, p, new PlayNextAttackThisTurnAdditionalTimesPower(p, magicNumber), magicNumber));
     }
 
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_DOUBLE_TAP_TURNS);
-            initializeDescription();
+            upgradeMagicNumber(UPGRADE_PLUS_NEXT_ATTACK_ADDITIONAL_TIMES);
+            upgradeDescription();
         }
     }
 }
---
+++ src/main/java/stsjorbsmod/powers/PlayNextAttackThisTurnAdditionalTimesPower.java
@@ -0,0 +1,91 @@
+package stsjorbsmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.util.TextureLoader;
+
+import static stsjorbsmod.JorbsMod.makePowerPath;
+
+// Similar to DoubleTapPower, except stacks as "next attack plays N times" rather than "next N attacks are played twice"
+public class PlayNextAttackThisTurnAdditionalTimesPower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = JorbsMod.makeID(PlayNextAttackThisTurnAdditionalTimesPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("play_next_attack_this_turn_additional_times_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("play_next_attack_this_turn_additional_times_power32.png"));
+
+    public PlayNextAttackThisTurnAdditionalTimesPower(final AbstractCreature owner, final int additionalTimes) {
+        ID = POWER_ID;
+        this.name = NAME;
+
+        this.owner = owner;
+        this.amount = additionalTimes;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (!card.purgeOnUse && card.type == AbstractCard.CardType.ATTACK && this.amount > 0) {
+            this.flash();
+
+            AbstractMonster m = (AbstractMonster)action.target;
+
+            for (int i = 0; i < amount; ++i) {
+                AbstractCard tmp = card.makeSameInstanceOf();
+                AbstractDungeon.player.limbo.addToBottom(tmp);
+                tmp.current_x = card.current_x;
+                tmp.current_y = card.current_y;
+                tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+                tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+                if (tmp.cost > 0) {
+                    tmp.freeToPlayOnce = true;
+                }
+
+                if (m != null) {
+                    tmp.calculateCardDamage(m);
+                }
+
+                tmp.purgeOnUse = true;
+                AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(tmp, m, card.energyOnUse, true));
+            }
+
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+        }
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = amount == 1 ? DESCRIPTIONS[0] : DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new PlayNextAttackThisTurnAdditionalTimesPower(owner, amount);
+    }
+}
+
