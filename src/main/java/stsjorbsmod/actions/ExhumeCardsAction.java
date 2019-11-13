2739c3ef68c2a70d97b88a68b55ae93b3c9609e1
Dan Bjorge <dan@dbjorge.net>
2019-11-13 00:40:31 -0800

    Make exhumed cards go to discard if they dont fit in hand

--- src/main/java/stsjorbsmod/actions/ExhumeCardsAction.java
+++ src/main/java/stsjorbsmod/actions/ExhumeCardsAction.java
@@ -1,6 +1,7 @@
 package stsjorbsmod.actions;
 
 import basemod.BaseMod;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
@@ -8,12 +9,15 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.CorruptionPower;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToDiscardEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
 import stsjorbsmod.cards.AutoExhumeBehavior;
 import stsjorbsmod.patches.AutoExhumeField;
 
 import java.util.function.Predicate;
 
 public class ExhumeCardsAction extends AbstractGameAction {
+    private static final float PADDING = 25.0F * Settings.scale;
     private final Predicate<AbstractCard> shouldApplyToCardPredicate;
 
     public ExhumeCardsAction(Predicate<AbstractCard> shouldApplyToCardPredicate) {
@@ -34,12 +38,6 @@ public class ExhumeCardsAction extends AbstractGameAction {
         AbstractPlayer p = AbstractDungeon.player;
         int roomInHand = BaseMod.MAX_HAND_SIZE - p.hand.size();
 
-        if (roomInHand == 0) {
-            p.createHandIsFullDialog();
-            isDone = true;
-            return;
-        }
-
         CardGroup targetCards = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
         for (AbstractCard c : p.exhaustPile.group) {
             if (shouldApplyToCardPredicate.test(c)) {
@@ -48,16 +46,19 @@ public class ExhumeCardsAction extends AbstractGameAction {
         }
 
         if (targetCards.size() > roomInHand) {
+            p.createHandIsFullDialog();
             targetCards.shuffle();
-            for (int i=targetCards.size(); i > roomInHand; --i) {
-                targetCards.removeTopCard();
-            }
         }
 
         for (AbstractCard c : targetCards.group) {
-            // This loop body taken from ExhumeAction
             c.unfadeOut();
-            p.hand.addToHand(c);
+            if (p.hand.size() < BaseMod.MAX_HAND_SIZE) {
+                AbstractDungeon.effectList.add(new ShowCardAndAddToHandEffect(c));
+            } else {
+                AbstractDungeon.effectList.add(new ShowCardAndAddToDiscardEffect(c));
+            }
+
+            // Special case per ExhumeAction
             if (AbstractDungeon.player.hasPower(CorruptionPower.POWER_ID) && c.type == AbstractCard.CardType.SKILL) {
                 c.setCostForTurn(-9);
             }
