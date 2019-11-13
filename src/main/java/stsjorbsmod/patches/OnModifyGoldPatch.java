e8585766727801f4d9bc85d1d0cbc21bde49eb77
James Wang <james.wang429@gmail.com>
2019-11-12 23:31:30 -0500

    fix by making the notify a combat phase only thing.

--- src/main/java/stsjorbsmod/patches/OnModifyGoldPatch.java
+++ src/main/java/stsjorbsmod/patches/OnModifyGoldPatch.java
@@ -5,24 +5,27 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import stsjorbsmod.memories.AbstractMemory;
 import stsjorbsmod.memories.MemoryManager;
 import stsjorbsmod.powers.IOnModifyGoldListener;
 
 public class OnModifyGoldPatch {
     private static void notifyModifyGold(AbstractPlayer player) {
-        for (AbstractPower p : player.powers) {
-            if (p instanceof IOnModifyGoldListener) {
-                ((IOnModifyGoldListener) p).onModifyGold(player);
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            for (AbstractPower p : player.powers) {
+                if (p instanceof IOnModifyGoldListener) {
+                    ((IOnModifyGoldListener) p).onModifyGold(player);
+                }
             }
-        }
-        MemoryManager memoryManager = MemoryManager.forPlayer(player);
-        if (memoryManager != null) {
-            for (AbstractMemory m : memoryManager.currentMemories()) {
-                m.onModifyGold(player);
+            MemoryManager memoryManager = MemoryManager.forPlayer(player);
+            if (memoryManager != null) {
+                for (AbstractMemory m : memoryManager.currentMemories()) {
+                    m.onModifyGold(player);
+                }
             }
+            AbstractDungeon.onModifyPower();
         }
-        AbstractDungeon.onModifyPower();
     }
 
     @SpirePatch(
