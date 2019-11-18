a444b856363b1cf44ac19b03afc7493a81ab163e
James Wang <james.wang429@gmail.com>
2019-11-18 01:36:10 -0500

    rediscovery

---
+++ src/main/java/stsjorbsmod/cards/wanderer/Rediscovery.java
@@ -0,0 +1,43 @@
+package stsjorbsmod.cards.wanderer;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Wanderer;
+import stsjorbsmod.powers.RediscoveryPower;
+
+import static stsjorbsmod.JorbsMod.makeCardPath;
+
+public class Rediscovery extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Rediscovery.class.getSimpleName());
+    public static final String IMG = makeCardPath("Scaling_Rares/rediscovery.png");
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Wanderer.Enums.WANDERER_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int UPGRADED_COST = 2;
+
+    public Rediscovery() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RediscoveryPower(p)));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            upgradeDescription();
+        }
+    }
+}
