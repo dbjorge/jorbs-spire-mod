75a8e6b6df7dfe251f85f3bbcab3d8016364fd35
James Wang <james.wang429@gmail.com>
2019-11-19 22:25:53 -0500

    amnesia does the right thing.

--- src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Amnesia.java
@@ -1,25 +1,28 @@
 package stsjorbsmod.cards.wanderer;
 
-import com.megacrit.cardcrawl.actions.unique.RemoveAllPowersAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 import stsjorbsmod.JorbsMod;
-import stsjorbsmod.actions.RemovePowersMatchingPredicateAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
-import stsjorbsmod.actions.SnapAction;
-import stsjorbsmod.characters.Wanderer;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static stsjorbsmod.JorbsMod.makeCardPath;
 
 /**
  * Curse
  * - Unplayable
- * - At the end of player turn, Snap.
- * - Remove player buffs and debuffs.
+ * - At the end of player turn, lose random buff
  */
 public class Amnesia extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(Amnesia.class.getSimpleName());
@@ -38,9 +41,19 @@ public class Amnesia extends CustomJorbsModCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster _) {
-        if(dontTriggerOnUseCard) {
-            addToBot(new SnapAction(p));
-            addToBot(new RemovePowersMatchingPredicateAction(p, power -> power.type == AbstractPower.PowerType.BUFF || power.type == AbstractPower.PowerType.DEBUFF));
+        if (dontTriggerOnUseCard) {
+            List<AbstractPower> buffs = p.powers.stream()
+                    .filter(power -> power.type == AbstractPower.PowerType.BUFF)
+                    .collect(Collectors.toList());
+            if (buffs.size() == 0) {
+                AbstractDungeon.actionManager.addToBottom(new TalkAction(true,"~...~", 2.0f, 2.0f));
+            } else if (p.hasPower(ArtifactPower.POWER_ID)) {
+                AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(p, p, ArtifactPower.POWER_ID, 1));
+            } else {
+                int index = AbstractDungeon.miscRng.random(buffs.size());
+                AbstractPower powerToRemove = buffs.get(index);
+                AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(p, p, powerToRemove.ID, powerToRemove.amount));
+            }
         }
     }
 
