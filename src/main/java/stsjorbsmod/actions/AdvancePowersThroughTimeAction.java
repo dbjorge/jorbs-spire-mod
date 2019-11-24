22b82e5f3329b807cd3ed38224c0086113ad3f96
Dan Bjorge <dan@dbjorge.net>
2019-11-23 20:21:31 -0800

    Time Eddy: Fix ordering/numbering of effects

--- src/main/java/stsjorbsmod/actions/AdvancePowersThroughTimeAction.java
+++ src/main/java/stsjorbsmod/actions/AdvancePowersThroughTimeAction.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AdvancePowersThroughTimeAction extends AbstractGameAction {
@@ -15,22 +16,24 @@ public class AdvancePowersThroughTimeAction extends AbstractGameAction {
         this.shouldAffectPowerPredicate = shouldAffectPowerPredicate;
     }
 
-    @Override
-    public void update() {
+    private void forEachApplicablePower(Consumer<AbstractPower> callback) {
         for (AbstractPower power : target.powers) {
             if (shouldAffectPowerPredicate.test(power)) {
-                if (target.isPlayer) {
-                    power.atEndOfTurnPreEndTurnCards(true);
-                    power.atEndOfTurn(true);
-                }
-                power.atEndOfTurnPreEndTurnCards(false);
-                power.atEndOfTurn(false);
-                power.atEndOfRound();
-                power.atStartOfTurn();
-                power.atStartOfTurnPostDraw();
+                callback.accept(power);
             }
         }
+    }
 
+    @Override
+    public void update() {
+        for (int i = 0; i < this.amount; ++i) {
+            forEachApplicablePower(p -> p.atEndOfTurnPreEndTurnCards(target.isPlayer));
+            forEachApplicablePower(p -> p.atEndOfTurn(target.isPlayer));
+            forEachApplicablePower(p -> p.atEndOfRound());
+            forEachApplicablePower(p -> p.atStartOfTurn());
+            forEachApplicablePower(p -> p.atStartOfTurnPostDraw());
+        }
+        
         isDone = true;
     }
 }
