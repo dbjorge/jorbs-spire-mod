59bdabe76245e2a9bb83eed45844d9f51a484925
Dan Bjorge <dan@dbjorge.net>
2019-11-24 13:23:02 -0800

    Scorching Ray: rework as simpler skill

--- src/main/java/stsjorbsmod/actions/ScorchingRayAction.java
+++ src/main/java/stsjorbsmod/actions/ScorchingRayAction.java
@@ -3,14 +3,13 @@ package stsjorbsmod.actions;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
 import stsjorbsmod.effects.ScalingLaserEffect;
-import stsjorbsmod.patches.DamageAsBurningPatch;
+import stsjorbsmod.powers.BurningPower;
 
 public class ScorchingRayAction extends AbstractGameAction {
     public ScorchingRayAction(AbstractCreature target, AbstractCreature source, int amount) {
@@ -26,9 +25,7 @@ public class ScorchingRayAction extends AbstractGameAction {
         AbstractDungeon.actionManager.addToBottom(new VFXAction(
                 new ScalingLaserEffect(source.hb.cX, source.hb.cX, target.hb.cX, target.hb.cY, Color.ORANGE.cpy(), Color.RED.cpy(), amount), 0.1F));
 
-        DamageInfo info = new DamageInfo(source, amount);
-        DamageAsBurningPatch.isBurningField.isBurning.set(info, true);
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(target, info, AttackEffect.FIRE));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(target, source, new BurningPower(target, source, amount)));
 
         isDone = true;
     }
