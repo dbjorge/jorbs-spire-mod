084737d3bf68c397f7916f6c5d7c32e7775fb113
Dan Bjorge <dan@dbjorge.net>
2019-12-04 20:27:35 -0800

    Fix CardBannerImageRarityPatch to also work on zoomed view

--- src/main/java/stsjorbsmod/patches/CardBannerImageRarityPatch.java
+++ src/main/java/stsjorbsmod/patches/CardBannerImageRarityPatch.java
@@ -5,6 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.FieldAccess;
@@ -17,20 +18,32 @@ public class CardBannerImageRarityPatch {
         return (c instanceof CustomJorbsModCard) ? ((CustomJorbsModCard)c).bannerImageRarity : c.rarity;
     }
 
+    public static class ReplaceRarityWithBannerRarityExprEditor extends ExprEditor {
+        @Override
+        public void edit(FieldAccess fieldAccess) throws CannotCompileException {
+            if (fieldAccess.getClassName().equals(AbstractCard.class.getName()) && fieldAccess.getFieldName().equals("rarity")) {
+                fieldAccess.replace(String.format("{ $_ = %1$s.getBannerRarity($0); }", CardBannerImageRarityPatch.class.getName()));
+            }
+        }
+    }
+
     @SpirePatch(
             clz = AbstractCard.class,
             method = "renderBannerImage"
     )
     public static class AbstractCard_renderBannerImage {
         public static ExprEditor Instrument() {
-            return new ExprEditor() {
-                @Override
-                public void edit(FieldAccess fieldAccess) throws CannotCompileException {
-                    if (fieldAccess.getClassName().equals(AbstractCard.class.getName()) && fieldAccess.getFieldName().equals("rarity")) {
-                        fieldAccess.replace(String.format("{ $_ = %1$s.getBannerRarity($0); }", CardBannerImageRarityPatch.class.getName()));
-                    }
-                }
-            };
+            return new ReplaceRarityWithBannerRarityExprEditor();
+        }
+    }
+
+    @SpirePatch(
+            clz = SingleCardViewPopup.class,
+            method = "renderCardBanner"
+    )
+    public static class SingleCardViewPopup_renderCardBanner {
+        public static ExprEditor Instrument() {
+            return new ReplaceRarityWithBannerRarityExprEditor();
         }
     }
 }
