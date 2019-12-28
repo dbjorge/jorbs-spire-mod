fdbefae13a57b6dd9f2df75107466c740df5131c
Cartopol <onmogelijk@gmail.com>
2019-12-29 00:13:25 +0100

    Corrected: - Updated to updated Power format - Updated string format for power description - Renamed power icons

--- src/main/java/stsjorbsmod/powers/SpiritShieldPower.java
+++ src/main/java/stsjorbsmod/powers/SpiritShieldPower.java
@@ -1,42 +1,23 @@
 package stsjorbsmod.powers;
 
-import basemod.interfaces.CloneablePowerInterface;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import stsjorbsmod.JorbsMod;
-import stsjorbsmod.util.TextureLoader;
 
-import static stsjorbsmod.JorbsMod.makePowerPath;
-
-public class SpiritShieldPower extends AbstractPower implements CloneablePowerInterface {
-    public static final String POWER_ID = JorbsMod.makeID(SpiritShieldPower.class.getSimpleName());
-    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
-    public static final String NAME = powerStrings.NAME;
-    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-
-    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("spirit_shield_power84.png"));
-    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("spirit_shield_power32.png"));
+public class SpiritShieldPower extends CustomJorbsModPower {
+    public static final StaticPowerInfo STATIC = StaticPowerInfo.Load(SpiritShieldPower.class);
+    public static final String POWER_ID = STATIC.ID;
 
     private static final int DAMAGE_REDUCTION = 1;
 
     public SpiritShieldPower(final AbstractCreature owner, final int numberTurns) {
-        ID = POWER_ID;
-        this.name = NAME;
+        super(STATIC);
 
         this.owner = owner;
         this.amount = numberTurns;
         this.isTurnBased = true;
-
-
-        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
-        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
         updateDescription();
     }
@@ -62,9 +43,7 @@ public class SpiritShieldPower extends AbstractPower implements CloneablePowerIn
 
     @Override
     public void updateDescription() {
-        description = this.amount <= 1 ?
-                DESCRIPTIONS[0] + DAMAGE_REDUCTION + DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2] :
-                (DESCRIPTIONS[0] + DAMAGE_REDUCTION + DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[3]);
+        description = String.format(this.amount == 1 ? DESCRIPTIONS[0] : DESCRIPTIONS[1], this.amount, DAMAGE_REDUCTION);
     }
 
     @Override
