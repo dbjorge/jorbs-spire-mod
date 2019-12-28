19347eda2ccb107cb51fc73955f2abe821453a24
Cartopol <onmogelijk@gmail.com>
2019-12-28 19:38:17 +0100

    Implements Cull Rare card Pathos 3: Cards in your hand cost 0 this turn. Add a curse to your discard pile for each card played.
    Currently sets card in hand to 0, and sets cards drawn to 0. Does not work on cards created in hand.

---
+++ src/main/java/stsjorbsmod/powers/PathosPower.java
@@ -0,0 +1,73 @@
+package stsjorbsmod.powers;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import java.util.Iterator;
+
+public class PathosPower extends CustomJorbsModPower {
+    public static final StaticPowerInfo STATIC = StaticPowerInfo.Load(PathosPower.class);
+    public static final String POWER_ID = STATIC.ID;
+
+    public PathosPower(final AbstractCreature owner, final int numberOfTurns) {
+        super(STATIC);
+
+        this.owner = owner;
+        this.amount = numberOfTurns;
+        this.isTurnBased = true;
+
+        updateDescription();
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        this.flash();
+        AbstractCard c = AbstractDungeon.returnRandomCurse();
+        addToBot(new MakeTempCardInDiscardAction(c, 1));
+    }
+
+    @Override
+    public void onInitialApplication() {
+        Iterator var1 = AbstractDungeon.player.hand.group.iterator();
+
+        while(var1.hasNext()) {
+            AbstractCard c = (AbstractCard)var1.next();
+            c.setCostForTurn(-9);
+        }
+    }
+
+    @Override
+    public void onCardDraw(AbstractCard card) {
+            card.setCostForTurn(-9);
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (this.amount == 0) {
+            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+        } else {
+            this.addToBot(new ReducePowerAction(this.owner, this.owner, this.ID, 1));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = String.format(DESCRIPTIONS[0], this.amount);
+        } else {
+            this.description = String.format(DESCRIPTIONS[1], this.amount);
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new PathosPower(owner, amount);
+    }
+}
+
