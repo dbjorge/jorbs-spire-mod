dc04028dde5628f880c707db39d50ff29a4e4427
Cartopol <onmogelijk@gmail.com>
2019-12-28 23:30:21 +0100

    Implements Cull basic card Wail 0: Apply 1(2) Vulnerable. Draw 1 Card.

--- src/main/java/stsjorbsmod/characters/Cull.java
+++ src/main/java/stsjorbsmod/characters/Cull.java
@@ -21,10 +21,7 @@ import com.megacrit.cardcrawl.relics.SpiritPoop;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import stsjorbsmod.cards.cull.Apparate;
-import stsjorbsmod.cards.cull.Apparition_Cull;
-import stsjorbsmod.cards.cull.CULLCard;
-import stsjorbsmod.cards.cull.Inhale;
+import stsjorbsmod.cards.cull.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -210,7 +207,7 @@ public class Cull extends CustomPlayer {
         // retVal.add(SpiritShield_Cull.ID);
         // retVal.add(Siphon.ID);
         // retVal.add(SplinterSoul.ID);
-        // retVal.add(Wail.ID);
+        retVal.add(Wail.ID);
         // retVal.add(WakingDream.ID);
 
         return retVal;
