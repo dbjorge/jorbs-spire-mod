5b6863b44f7f4b4e293cf10a231f237e70e25d93
Cartopol <59176413+Cartopol@users.noreply.github.com>
2019-12-28 23:14:34 +0100

    Implemented Cull Rare card: Consumption (#264)
    * Implemented Cull Rare card: Consumption
    X: Deal X damage, gain max hp equal to damage dealt. Exhaust.
    
    Damage and MaxHP gain scale with all damage modifiers (strength, vuln, pen nib etc.). MaxHP gain is equal to the actual hp loss of the target, and as such is blocked by shield, and capped by the actual HP of the target.
    
    One note, I couldn't find a clean way to add dynamic text, since the damage is calculated in use() based on energy spent. Feel free to give implementation suggestions as feedback.
    
    * Results of experimenting with energyOnUse
    
    Co-authored-by: Dan Bjorge <dan@dbjorge.net>

---
+++ src/main/java/stsjorbsmod/actions/DrainLifeAction.java
@@ -0,0 +1,43 @@
+package stsjorbsmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+
+public class DrainLifeAction extends AbstractGameAction {
+    private DamageInfo info;
+
+    public DrainLifeAction(AbstractCreature target, DamageInfo info) {
+        this.info = info;
+        this.setValues(target, info);
+        this.actionType = ActionType.DAMAGE;
+        this.startDuration = Settings.ACTION_DUR_FAST;
+        this.duration = this.startDuration;
+    }
+
+    public void update() {
+        if (this.shouldCancelAction()) {
+            this.isDone = true;
+        } else {
+            this.tickDuration();
+            if (this.isDone) {
+                AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.FIRE, false));
+                this.target.damage(this.info);
+                if (this.target.lastDamageTaken > 0) {
+                    AbstractDungeon.player.increaseMaxHp(this.target.lastDamageTaken, true);
+                }
+
+                if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                    AbstractDungeon.actionManager.clearPostCombatActions();
+                } else {
+                    this.addToTop(new WaitAction(0.1F));
+                }
+            }
+
+        }
+    }
+}
