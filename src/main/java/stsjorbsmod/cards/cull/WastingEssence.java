993c3891ff408c50b607f04d221a700e867e5ac8
Cartopol <onmogelijk@gmail.com>
2019-12-29 15:38:05 +0100

    Bugfix #281: Additional Wasting Essence do not add curses when played - moved curse creation from WastingEssencePower method onInitialApplication to Wasting Essence use method

--- src/main/java/stsjorbsmod/cards/cull/WastingEssence.java
+++ src/main/java/stsjorbsmod/cards/cull/WastingEssence.java
@@ -2,7 +2,10 @@ package stsjorbsmod.cards.cull;
 
 import basemod.helpers.BaseModCardTags;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
@@ -33,6 +36,10 @@ public class WastingEssence extends CustomJorbsModCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < metaMagicNumber; i++) {
+            AbstractCard c = AbstractDungeon.returnRandomCurse();
+            AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDiscardAction(c, 1));
+        }
         addToBot(new ApplyPowerAction(p, p, new WastingEssencePower(p, magicNumber, metaMagicNumber)));
     }
 
