3c0dc7ccd2e67b0e40ac2e6be3cc919998db1d66
Cartopol <onmogelijk@gmail.com>
2019-12-29 23:48:25 +0100

    - Cleaned up ID references - Added check on empty hand in case it is played from another zone (eg. from top of deck with a havoc)

---
+++ src/main/java/stsjorbsmod/cards/cull/Ritual.java
@@ -0,0 +1,53 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.unique.RetainCardsAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EquilibriumPower;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import com.megacrit.cardcrawl.relics.RunicPyramid;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+public class Ritual extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Ritual.class);
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int INTANGIBLE = 1;
+    private static final int CARDS_TO_RETAIN = 1;
+    private static final int UPGRADE_CARDS_TO_RETAIN = 1;
+
+    public Ritual() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = CARDS_TO_RETAIN;
+        urMagicNumber = baseUrMagicNumber = INTANGIBLE;
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new IntangiblePlayerPower(p, urMagicNumber)));
+        // This makes sure that choice window does not pop-up when this is the last card played from hand
+        if (!(p.hand.size() == 1 && p.hand.group.get(0) == this) && !p.hand.isEmpty() && !AbstractDungeon.player.hasRelic(RunicPyramid.ID) && !AbstractDungeon.player.hasPower(EquilibriumPower.POWER_ID)) {
+            this.addToBot(new RetainCardsAction(p, magicNumber));
+        }
+
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_CARDS_TO_RETAIN);
+            upgradeDescription();
+        }
+    }
+}
