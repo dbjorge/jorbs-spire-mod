5cb3e47a49fdc372f16a9328341e2f16c29ca5b4
Cartopol <onmogelijk@gmail.com>
2020-01-01 18:33:47 +0100

    Implements Cull common Strife 1: Add a curse to your discard pile. Gain 1 strength for every curse in your hand/discard pile/draw pile

---
+++ src/main/java/stsjorbsmod/cards/cull/Strife.java
@@ -0,0 +1,70 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+public class Strife extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Strife.class);
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int STRENGTH_PER_CURSE = 1;
+    private static final int UPGRADE_STRENGTH_PER_CURSE = 1;
+    private static final int CURSE_AMOUNT = 1;
+
+    public Strife() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = STRENGTH_PER_CURSE;
+    }
+
+    private void calculateCurseAmount() {
+        int nbCurses = CURSE_AMOUNT;
+        AbstractPlayer p = AbstractDungeon.player;
+        for (AbstractCard card : p.drawPile.group) {
+            if (card.rarity.equals(CardRarity.CURSE)) {
+                ++nbCurses;
+            }
+        }
+        for (AbstractCard card : p.discardPile.group) {
+            if (card.rarity.equals(CardRarity.CURSE)) {
+                ++nbCurses;
+            }
+        }
+        for (AbstractCard card : p.hand.group) {
+            if (card.rarity.equals(CardRarity.CURSE)) {
+                ++nbCurses;
+            }
+        }
+        urMagicNumber = nbCurses * magicNumber;
+    }
+
+        @Override
+    public void use(AbstractPlayer p, AbstractMonster abstractMonster) {
+        AbstractCard c = AbstractDungeon.returnRandomCurse();
+        addToBot(new MakeTempCardInDiscardAction(c, CURSE_AMOUNT));
+        calculateCurseAmount();
+        addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, urMagicNumber)));
+    }
+
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_STRENGTH_PER_CURSE);
+            upgradeDescription();
+        }
+    }
+}
