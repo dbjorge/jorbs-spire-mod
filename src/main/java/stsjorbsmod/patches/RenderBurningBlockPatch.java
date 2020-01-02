94a73ab49031c717f2546280c6416e5e6fa97dca
James Wang <james.wang429@gmail.com>
2020-01-01 19:37:18 -0500

    prefix patch

--- src/main/java/stsjorbsmod/patches/RenderBurningBlockPatch.java
+++ src/main/java/stsjorbsmod/patches/RenderBurningBlockPatch.java
@@ -3,38 +3,28 @@ package stsjorbsmod.patches;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
-import javassist.CannotCompileException;
-import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
-import stsjorbsmod.JorbsMod;
-import stsjorbsmod.powers.BurningPower;
+import stsjorbsmod.util.ReflectionUtils;
 
 public class RenderBurningBlockPatch {
-    @SpirePatch(clz = AbstractCreature.class, method = "renderHealth")
-    public static class AbstractCreature_renderHealthPatch {
-        public static ExprEditor Instrument() {
-            return new ExprEditor() {
-                @Override
-                public void edit(MethodCall m) throws CannotCompileException {
-                    if (m.getClassName().contains(AbstractCreature.class.getName()) && m.getMethodName().equals("renderBlockIconAndValue")) {
-                        m.replace(String.format("{ if (this.hasPower(\"%1$s\")) {" +
-                                        "%2$s.renderBurningBlock($$, %3$s#BLOCK_ICON_X, %3$s#BLOCK_ICON_Y, this.blockOffset);" +
-                                        "} $_ = $proceed($$); }",
-                                JorbsMod.makeID(BurningPower.class.getSimpleName()),
-                                RenderBurningBlockPatch.class.getName(),
-                                AbstractCreature.class.getName()));
-                    }
-                }
-            };
+    @SpirePatch(clz = AbstractCreature.class, method = "renderBlockIconAndValue")
+    public static class AbstractCreature_renderBlockIconAndValue {
+        @SpirePrefixPatch
+        public static void patch(AbstractCreature __this, SpriteBatch sb, float x, float y) {
+            sb.setColor(Color.GOLD.cpy());
+            sb.draw(ImageMaster.ATK_FIRE,
+                    x + (Float)ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "BLOCK_ICON_X") - 32.0F,
+                    y + (Float)ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "BLOCK_ICON_Y") - 32.0F + (Float)ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "blockOffset"),
+                    32.0F,
+                    32.0F,
+                    64.0F,
+                    64.0F,
+                    Settings.scale,
+                    Settings.scale,
+                    0.0F);
         }
-    }
-
-
-    public static void renderBurningBlock(SpriteBatch sb, float x, float y, float BLOCK_ICON_X, float BLOCK_ICON_Y, float blockOffset) {
-        sb.setColor(Color.GOLD.cpy());
-        sb.draw(ImageMaster.ATK_FIRE, x + BLOCK_ICON_X - 32.0F, y + BLOCK_ICON_Y - 32.0F + blockOffset, 32.0F, 32.0F, 64.0F, 64.0F, Settings.scale, Settings.scale, 0.0F);
     }
 }
