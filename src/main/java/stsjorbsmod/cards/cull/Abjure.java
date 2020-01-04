bc61c635da2ee7e7859b7eac9faf10851ddea120
Cartopol <onmogelijk@gmail.com>
2020-01-04 01:18:47 +0100

    Cleaned up unnecessary change.

--- src/main/java/stsjorbsmod/cards/cull/Abjure.java
+++ src/main/java/stsjorbsmod/cards/cull/Abjure.java
@@ -30,9 +30,6 @@ public class Abjure extends CustomJorbsModCard {
     @Override
     public void upgrade() {
         if (!upgraded) {
-            //until beta releases, replace the following line with the commented line
-            AlwaysRetainField.alwaysRetain.set(this, true);
-//            this.selfRetain = true;
             // TODO: Replace with selfRetain on beta release
             AlwaysRetainField.alwaysRetain.set(this, true);
 
