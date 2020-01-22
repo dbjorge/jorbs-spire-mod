2e0020faeebbb88e1562acf992124a79a3c68b50
Cartopol <onmogelijk@gmail.com>
2020-01-22 19:23:49 +0100

    Corrections after review - Corrected power strings - Changed damage type to Thorns

--- src/main/java/stsjorbsmod/powers/MirrorPower.java
+++ src/main/java/stsjorbsmod/powers/MirrorPower.java
@@ -5,11 +5,12 @@ import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class MirrorPower extends CustomJorbsModPower {
+public class MirrorPower extends CustomJorbsModPower implements OnDamageToRedirectSubscriber {
     public static final StaticPowerInfo STATIC = StaticPowerInfo.Load(MirrorPower.class);
     public static final String POWER_ID = STATIC.ID;
 
@@ -33,12 +34,13 @@ public class MirrorPower extends CustomJorbsModPower {
     }
 
     @Override
-    public int onAttacked(DamageInfo info, int damageAmount) {
+    public boolean onDamageToRedirect(AbstractPlayer player, DamageInfo info, AbstractGameAction.AttackEffect attackEffect) {
         if (info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != null && info.owner != this.owner) {
             this.flash();
-            this.addToTop(new DamageAction(info.owner, new DamageInfo(this.owner, damageAmount, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, false));
+            this.addToTop(new DamageAction(info.owner, new DamageInfo(this.owner, info.output, DamageInfo.DamageType.THORNS), attackEffect, false));
+            return true;
         }
-        return damageAmount;
+        return false;
     }
 
     @Override
