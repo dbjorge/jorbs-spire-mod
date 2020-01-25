d631845e22e3bd2e08eba9d56a7bd74c36b341d1
James Wang <james.wang429@gmail.com>
2020-01-25 02:40:45 -0500

    move fire to between block icon and block number (#311)
    * move fire to between block icon and block number
    
    * make the burning icon mix with red
    
    * burning power icon, burning block color based on percent burning, spinny icon
    
    * review comments
    
    * trimming unneeded bits

--- src/main/java/stsjorbsmod/patches/BurningPatch.java
+++ src/main/java/stsjorbsmod/patches/BurningPatch.java
@@ -1,14 +1,16 @@
 package stsjorbsmod.patches;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.common.EndTurnAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
@@ -40,15 +42,22 @@ public class BurningPatch {
         }
     }
 
+    static TextureAtlas.AtlasRegion BURNING_TEXTURE = new TextureAtlas(Gdx.files.internal("powers/powers.atlas")).findRegion("128/attackBurn");
+    static float ROTATION_DURATION = 60.0F;
+
+    @SpirePatch(clz = AbstractCreature.class, method = SpirePatch.CLASS)
+    public static class AbstractCreatureBurningFields {
+        public static SpireField<Float> burningRotation = new SpireField<>(() -> 0.0F);
+    }
 
     @SpirePatch(clz = AbstractCreature.class, method = "renderBlockIconAndValue")
-    public static class AbstractCreature_renderBlockIconAndValue {
+    public static class AbstractCreature_renderBlockIconAndValue_showFire {
         @SpirePrefixPatch
-
         public static void patch(AbstractCreature __this, SpriteBatch sb, float x, float y) {
             if (__this.hasPower(BurningPower.POWER_ID)) {
-                sb.setColor(Color.GOLD.cpy());
-                sb.draw(ImageMaster.ATK_FIRE,
+                sb.setColor(Color.WHITE.cpy());
+                float burningRotation = AbstractCreatureBurningFields.burningRotation.get(__this) / ROTATION_DURATION * 360.0F;
+                sb.draw(BURNING_TEXTURE,
                         x + (Float) ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "BLOCK_ICON_X") - 32.0F,
                         y + (Float) ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "BLOCK_ICON_Y") - 32.0F + (Float) ReflectionUtils.getPrivateField(__this, AbstractCreature.class, "blockOffset"),
                         32.0F,
@@ -57,7 +66,41 @@ public class BurningPatch {
                         64.0F,
                         Settings.scale,
                         Settings.scale,
-                        0.0F);
+                        burningRotation);
+            }
+        }
+
+        public static ExprEditor Instrument() {
+            return new ExprEditor() {
+                @Override
+                public void edit(FieldAccess f) throws CannotCompileException {
+                    if (f.getClassName().contains(AbstractCreature.class.getName()) && f.getFieldName().equals("blockColor")) {
+                        f.replace(String.format("{ $_ = %1$s.renderBurningBlock(this, $proceed()); }",
+                                BurningPatch.class.getName()));
+                    }
+                }
+            };
+        }
+    }
+
+    public static Color renderBurningBlock(AbstractCreature creature, Color blockColor) {
+        if (creature != null && creature.hasPower(BurningPower.POWER_ID)) {
+            float lerpCoefficient = ((float) creature.getPower(BurningPower.POWER_ID).amount) / creature.currentBlock * 0.3F + 0.1F;
+            return blockColor.cpy().lerp(Color.RED.cpy(), lerpCoefficient);
+        } else {
+            return blockColor;
+        }
+    }
+
+    @SpirePatch(clz = AbstractCreature.class, method = "updateAnimations")
+    public static class AbstractCreature_updateBurningRotationTimer {
+        @SpirePrefixPatch
+        public static void patch(AbstractCreature __this) {
+            if (__this.hasPower(BurningPower.POWER_ID)) {
+                AbstractCreatureBurningFields.burningRotation.set(__this, AbstractCreatureBurningFields.burningRotation.get(__this) - Gdx.graphics.getDeltaTime());
+                if (AbstractCreatureBurningFields.burningRotation.get(__this) < 0.0F) {
+                    AbstractCreatureBurningFields.burningRotation.set(__this, ROTATION_DURATION);
+                }
             }
         }
     }
