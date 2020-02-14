76272279054c161dfc4a5456035695e9f3225d8a
Dan Bjorge <dan@dbjorge.net>
2020-02-13 20:29:13 -0800

    Refactor RemoveUnwantedBaseRelicsPatch slightly

--- src/main/java/stsjorbsmod/patches/RemoveUnwantedBaseRelicsPatch.java
+++ src/main/java/stsjorbsmod/patches/RemoveUnwantedBaseRelicsPatch.java
@@ -9,20 +9,20 @@ import stsjorbsmod.characters.Cull;
 import stsjorbsmod.characters.Wanderer;
 
 public class RemoveUnwantedBaseRelicsPatch {
-    @SpirePatch(
-            clz = AbstractDungeon.class,
-            method = "initializeRelicList"
-    )
+    @SpirePatch(clz = AbstractDungeon.class, method = "initializeRelicList")
     public static class Implementation
     {
         @SpirePrefixPatch
         public static void patch(AbstractDungeon __instance) {
-            boolean cull = false;
-            if (__instance.player instanceof Wanderer || (cull = __instance.player instanceof Cull)) {
-                AbstractDungeon.relicsToRemoveOnStart.add(PrismaticShard.ID);
+            boolean cull = __instance.player instanceof Cull;
+            boolean wanderer = __instance.player instanceof Wanderer;
 
-                // Cull has this implicitly
-                if (cull) { AbstractDungeon.relicsToRemoveOnStart.add(WingBoots.ID); }
+            if (wanderer || cull) {
+                AbstractDungeon.relicsToRemoveOnStart.add(PrismaticShard.ID);
+            }
+
+            if (cull) {
+                AbstractDungeon.relicsToRemoveOnStart.add(WingBoots.ID); // CULL has this implicitly
             }
         }
     }
