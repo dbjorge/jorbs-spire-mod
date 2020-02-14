dfc12d5a782e6d466c97c8d9e3d992af1118295e
James Wang <james.wang429@gmail.com>
2020-02-14 02:09:39 -0500

    Wrath icon relocated to under energy (#391)
    * Wrath icon relocated to under energy
    
    * comment
    
    * Use reddish text and incorporate icon movement into original image
    
    Co-authored-by: Dan Bjorge <dan@dbjorge.net>

--- src/main/java/stsjorbsmod/util/RenderUtils.java
+++ src/main/java/stsjorbsmod/util/RenderUtils.java
@@ -3,6 +3,7 @@ package stsjorbsmod.util;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.core.Settings;
 
 public class RenderUtils {
@@ -27,15 +28,21 @@ public class RenderUtils {
     }
 
     public static void renderAtlasRegionCenteredAt(SpriteBatch sb, TextureAtlas.AtlasRegion atlasRegion, float x, float y, float scale, Color color, float rotation) {
+        renderAtlasRegionCenteredAt(sb, atlasRegion, x, y, atlasRegion.offsetX, atlasRegion.offsetY, scale, color, rotation);
+    }
+
+    public static void renderAtlasRegionCenteredAt(SpriteBatch sb, TextureAtlas.AtlasRegion atlasRegion, float x, float y, float offsetX, float offsetY, float scale, Color color, float rotation) {
         sb.setColor(color);
+        float offsetXDrawn = offsetX * scale * MathUtils.cosDeg(rotation) + offsetY * scale * MathUtils.sinDeg(rotation);
+        float offsetYDrawn = offsetX * scale * MathUtils.sinDeg(rotation) - offsetY * scale * MathUtils.cosDeg(rotation);
         sb.draw(
                 atlasRegion,
-                x + atlasRegion.offsetX - (float)atlasRegion.packedWidth / 2.0F,
-                y + atlasRegion.offsetY - (float)atlasRegion.packedHeight / 2.0F,
-                (float)atlasRegion.packedWidth / 2.0F - atlasRegion.offsetX,
-                (float)atlasRegion.packedHeight / 2.0F - atlasRegion.offsetY,
-                (float)atlasRegion.packedWidth,
-                (float)atlasRegion.packedHeight,
+                x + atlasRegion.offsetX - (float) atlasRegion.packedWidth / 2.0F + offsetXDrawn,
+                y + atlasRegion.offsetY - (float) atlasRegion.packedHeight / 2.0F + offsetYDrawn,
+                (float) atlasRegion.packedWidth / 2.0F - atlasRegion.offsetX,
+                (float) atlasRegion.packedHeight / 2.0F - atlasRegion.offsetY,
+                (float) atlasRegion.packedWidth,
+                (float) atlasRegion.packedHeight,
                 scale,
                 scale,
                 rotation);
