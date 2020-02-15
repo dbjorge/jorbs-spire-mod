89189077a6b117a5ee45c2deafd2a20491405def
James Wang <007jcw@gmail.com>
2020-02-14 22:40:16 -0500

    banish interactions with curiosity, generic strength up, and anger powers

--- src/main/java/stsjorbsmod/patches/BanishPatch.java
+++ src/main/java/stsjorbsmod/patches/BanishPatch.java
@@ -5,10 +5,18 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.powers.TimeWarpPower;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.*;
 import stsjorbsmod.powers.BanishedPower;
 
 public class BanishPatch {
+    public static SpireReturn returnEarlyIfBanished(AbstractCreature c) {
+        if (c.hasPower(BanishedPower.POWER_ID)) {
+            return SpireReturn.Return(null);
+        }
+        return SpireReturn.Continue();
+    }
+
     @SpirePatch(
             clz = TimeWarpPower.class,
             method = "onAfterUseCard"
@@ -16,10 +24,24 @@ public class BanishPatch {
     public static class TimeWarpPower_onAfterUseCard {
         @SpirePrefixPatch
         public static SpireReturn prefix(TimeWarpPower __this, AbstractCard card, UseCardAction action) {
-            if (__this.owner.hasPower(BanishedPower.POWER_ID)) {
-                return SpireReturn.Return(null);
-            }
-            return SpireReturn.Continue();
+            return returnEarlyIfBanished(__this.owner);
+        }
+    }
+
+    @SpirePatch(clz = GenericStrengthUpPower.class, method = "atEndOfRound")
+    public static class GenericStrengthUpPower_AtEndOfRound {
+        @SpirePrefixPatch
+        public static SpireReturn prefix(GenericStrengthUpPower __this) {
+            return returnEarlyIfBanished(__this.owner);
+        }
+    }
+
+    @SpirePatch(clz = AngerPower.class, method = "onUseCard")
+    @SpirePatch(clz = CuriosityPower.class, method = "onUseCard")
+    public static class Power_onUseCard {
+        @SpirePrefixPatch
+        public static SpireReturn prefix(AbstractPower __this, AbstractCard card, UseCardAction action) {
+            return returnEarlyIfBanished(__this.owner);
         }
     }
 }
