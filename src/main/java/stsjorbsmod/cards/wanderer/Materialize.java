8e299b26171c71a198d7b260ba019f803410cb6f
James Wang <007jcw@gmail.com>
2020-02-15 16:10:50 -0500

    materialize costs 1, +1 block

--- src/main/java/stsjorbsmod/cards/wanderer/Materialize.java
+++ src/main/java/stsjorbsmod/cards/wanderer/Materialize.java
@@ -17,8 +17,8 @@ public class Materialize extends CustomJorbsModCard {
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Wanderer.Enums.WANDERER_CARD_COLOR;
 
-    private static final int COST = 0;
-    private static final int BLOCK = 4;
+    private static final int COST = 1;
+    private static final int BLOCK = 5;
     private static final int UPGRADE_PLUS_BLOCK = 2;
 
     public Materialize() {
