cabeee7766d359a20a46f6bd70ec9ea2512880a1
James Wang <007jcw@gmail.com>
2020-02-16 21:38:46 -0500

    give the ray of frost the ray effect, similar to scorching ray

--- src/main/java/stsjorbsmod/cards/wanderer/RayOfFrost.java
+++ src/main/java/stsjorbsmod/cards/wanderer/RayOfFrost.java
@@ -1,15 +1,22 @@
 package stsjorbsmod.cards.wanderer;
 
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
 import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.RayOfFrostAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.characters.Wanderer;
+import stsjorbsmod.effects.ScalingLaserEffect;
 
 public class RayOfFrost extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(RayOfFrost.class);
@@ -37,7 +44,7 @@ public class RayOfFrost extends CustomJorbsModCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage), AttackEffect.BLUNT_LIGHT));
+        addToBot(new RayOfFrostAction(m, new DamageInfo(p, damage)));
 
         for (int i=0; i<this.metaMagicNumber; ++i) {
             addToBot(new ApplyPowerAction(m, p, new WeakPower(m, this.magicNumber, false), this.magicNumber));
