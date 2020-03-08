359dfc87feaa658f4996d8d104216b6968e0e2dc
Cartopol <onmogelijk@gmail.com>
2020-03-08 19:53:58 +0100

    Implements Cull card Grim Dirge Entombed. Exhume on turn 3. 2: Deal 30 damage. Ephemeral
    Implementation modelled after Forbidden Grimoire; each copy of Grim Dirge in your deck will give you an EntombedGrimDirgePower instance that exhumes its copy of Grim Dirge to your hand.

--- src/main/java/stsjorbsmod/cards/cull/GrimDirge.java
+++ src/main/java/stsjorbsmod/cards/cull/GrimDirge.java
@@ -1,17 +1,23 @@
 package stsjorbsmod.cards.cull;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.cards.OnEntombedSubscriber;
 import stsjorbsmod.characters.Cull;
 import stsjorbsmod.patches.EntombedField;
 import stsjorbsmod.patches.EphemeralField;
+import stsjorbsmod.powers.EntombedGrimDirgePower;
 
-public class GrimDirge extends CustomJorbsModCard {
+public class GrimDirge extends CustomJorbsModCard implements OnEntombedSubscriber {
     public static final String ID = JorbsMod.makeID(GrimDirge.class);
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
@@ -22,7 +28,10 @@ public class GrimDirge extends CustomJorbsModCard {
     private static final int COST = 2;
     private static final int DAMAGE = 30;
     private static final int UPGRADE_DAMAGE = 10;
-    private static final int TURN_EXHUMED = 3;
+    private static final int EXHUME_TURN = 3;
+
+    private String grimDirgePowerInstanceID;
+
 
 
     public GrimDirge() {
@@ -30,13 +39,27 @@ public class GrimDirge extends CustomJorbsModCard {
         EntombedField.entombed.set(this, true);
         EphemeralField.ephemeral.set(this, true);
         baseDamage = DAMAGE;
-        baseMagicNumber = TURN_EXHUMED;
+        baseMagicNumber = EXHUME_TURN;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+    }
 
+
+    @Override
+    public void onCardEntombed() {
+        AbstractPower grimDirgePower = new EntombedGrimDirgePower(AbstractDungeon.player, this, EXHUME_TURN);
+        grimDirgePowerInstanceID = grimDirgePower.ID;
+        addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, grimDirgePower));
+    }
+
+    @Override
+    public AbstractCard makeStatEquivalentCopy() {
+        AbstractCard c = super.makeStatEquivalentCopy();
+        ((GrimDirge) c).grimDirgePowerInstanceID = grimDirgePowerInstanceID;
+        return c;
     }
 
     @Override
