510f5d686f1a488c508152b9477963122ad4585d
ajwhite81 <alex.white81@gmail.com>
2020-03-10 21:40:35 -0700

    Implement new CULL Card: Feint

---
+++ src/main/java/stsjorbsmod/cards/cull/Feint.java
@@ -0,0 +1,48 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+public class Feint extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Feint.class);
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_DAMAGE = 4;
+    private static final int DISCARD = 1;
+    private static final int DRAW = 2;
+
+
+    public Feint() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(m, baseDamage)));
+        addToBot(new DrawCardAction(p, DRAW));
+        addToBot(new DiscardAction(p, p, DISCARD, false, false));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeName();
+            upgradeDescription();
+        }
+    }
+}
