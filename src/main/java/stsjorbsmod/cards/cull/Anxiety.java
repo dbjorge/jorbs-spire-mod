4dc327408da8e03cd3089c5d17f6650322127754
Cartopol <onmogelijk@gmail.com>
2020-03-11 21:33:37 +0100

    Implements Cull card Anxiety 0: Draw 3 cards and gain 1 energy. Exhaust. Permanently draws one less card when played.
    Currently, always says 'Draw !M! cards' (plural). Not sure how to best make this base description dynamic. Move everything to Extended Description array?
    Also not sure if the handling of permanent property change is the most efficient. Felt like a lot of fiddling until it worked, so good chance there's some redundant code.

---
+++ src/main/java/stsjorbsmod/cards/cull/Anxiety.java
@@ -0,0 +1,62 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.defect.IncreaseMiscAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+public class Anxiety extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Anxiety.class);
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int INITIAL_DRAW = 3;
+    private static final int UPGRADE_DRAW = 1;
+
+    private static final int ENERGY = 1;
+    private static final int DRAW_LOSS = 1;
+
+    public Anxiety() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseMetaMagicNumber = metaMagicNumber = INITIAL_DRAW;
+        this.misc = metaMagicNumber;
+        baseMagicNumber = magicNumber = DRAW_LOSS;
+        baseUrMagicNumber = urMagicNumber = ENERGY;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster abstractMonster) {
+        addToBot(new DrawCardAction(this.misc));
+        addToBot(new GainEnergyAction(this.urMagicNumber));
+        if (this.misc > 0) {
+            this.addToBot(new IncreaseMiscAction(this.uuid, this.misc, -this.magicNumber));
+        }
+    }
+
+    public void applyPowers() {
+        this.baseMetaMagicNumber = this.misc;
+        super.applyPowers();
+        this.initializeDescription();
+    }
+
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeMetaMagicNumber(UPGRADE_DRAW);
+            ++this.misc;
+
+            upgradeName();
+            upgradeDescription();
+        }
+    }
+}
