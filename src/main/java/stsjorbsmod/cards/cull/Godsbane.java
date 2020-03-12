5de9b2f1e96b3b545ec0e8142f02d46af09cd89a
ajwhite81 <alex.white81@gmail.com>
2020-03-11 18:19:08 -0700

    Implement CULL Card: Godsbane

---
+++ src/main/java/stsjorbsmod/cards/cull/Godsbane.java
@@ -0,0 +1,53 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+
+public class Godsbane extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Godsbane.class);
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE_PERCENT = 8;
+    private static final int UPGRADE_DAMAGE_PERCENT = 2;
+
+    public Godsbane() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DAMAGE_PERCENT;
+        damage = baseDamage = 0;
+    }
+
+    private void recalculateDamage(AbstractMonster m) {
+        damage = (int) Math.round((m.maxHealth * ((double) magicNumber / 100)));
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster m) {
+        super.calculateCardDamage(m);
+        recalculateDamage(m);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage)));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeMagicNumber(UPGRADE_DAMAGE_PERCENT);
+            upgradeName();
+            upgradeDescription();
+        }
+    }
+}
