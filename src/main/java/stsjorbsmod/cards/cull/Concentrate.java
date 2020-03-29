10e7320e21aee78885bb50f8406d175f9c783d4b
Dan Bjorge <dan@dbjorge.net>
2020-03-29 14:33:04 -0700

    Clean up formatting

--- src/main/java/stsjorbsmod/cards/cull/Concentrate.java
+++ src/main/java/stsjorbsmod/cards/cull/Concentrate.java
@@ -8,7 +8,6 @@ import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.characters.Cull;
 
-
 public class Concentrate extends CustomJorbsModCard {
     public static final String ID = JorbsMod.makeID(Concentrate.class);
 
@@ -19,25 +18,28 @@ public class Concentrate extends CustomJorbsModCard {
 
     private static final int COST = 0;
 
+    private static final int ENERGY_GAIN = 2;
+    private static final int DISCARD = 3;
+    private static final int UPGRADE_PLUS_DISCARD = -1;
+
     public Concentrate() {
         super(ID, COST, TYPE, COLOR, RARITY, TARGET);
-        this.baseMagicNumber = 3;
+        this.baseMagicNumber = DISCARD;
         this.magicNumber = this.baseMagicNumber;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DiscardAction(p, p, this.magicNumber, false));
-        this.addToBot(new GainEnergyAction(2));
+        this.addToBot(new GainEnergyAction(ENERGY_GAIN));
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeMagicNumber(-1);
+            upgradeMagicNumber(UPGRADE_PLUS_DISCARD);
             upgradeDescription();
         }
-
     }
 }
