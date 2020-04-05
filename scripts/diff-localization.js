5cb88235ea72b4895e420794e6b53c26a43dcc5b
Dan Bjorge <dan@dbjorge.net>
2020-04-05 15:22:14 -0700

    Improve output options for diff-localization

--- scripts/diff-localization.js
+++ scripts/diff-localization.js
@@ -4,14 +4,12 @@
 // * old-git-tag and new-git-tag are "git commit-ish"es
 
 const child_process = require('child_process');
-const fs = require('fs');
 const jsonDiff = require('json-diff');
 const path = require('path');
 const process = require('process');
-const util = require('util');
 
-if (process.argv.length != 6) {
-    console.log('Usage: node diff-localization.js old-lang new-lang old-git-tag new-git-tag');
+if (process.argv.length < 6 || process.argv.length > 7 || (process.argv.length === 7 && process.argv[6] !== '--full')) {
+    console.log('Usage: node diff-localization.js old-lang new-lang old-git-tag new-git-tag [--full]');
     process.exit(1);
 }
 
@@ -20,6 +18,7 @@ const newLang = process.argv[3];
 const oldCommit = process.argv[4];
 const newCommit = process.argv[5];
 
+const outputFullDiff = process.argv.includes('--full');
 const structureOnly = (oldLang !== newLang);
 
 console.log(`COMPARING ${oldLang}@${oldCommit} vs ${newLang}@${newCommit}`);
@@ -71,6 +70,22 @@ function getFile(commit, lang, file) {
     return JSON.parse(gitOutput.toString());
 }
 
+function getKeywordName(keywordObj) {
+    if (keywordObj.PROPER_NAME != undefined) {
+        return keywordObj.PROPER_NAME.__old ? keywordObj.PROPER_NAME.__old : keywordObj.PROPER_NAME;
+    } else {
+        return keywordObj.NAMES[0];
+    }
+}
+
+function logKeys(code, keys) {
+    if (keys.length > 0) {
+        console.log(code === '-' ? '  DELETED:' : code === '+' ? '  ADDED:' : '  CHANGED:');
+        keys.forEach(k => console.log(`${code}   ${k.replace(/\u00a0/g, '\\u00a0')}`));
+    }
+}
+
+const colorDiffOpts = { color: false };
 const fileDiffOpts = { keysOnly: structureOnly };
 
 for (file of commonFiles) {
@@ -78,11 +93,42 @@ for (file of commonFiles) {
     newFile = getFile(newCommit, newLang, file);
     const diff = jsonDiff.diff(oldFile, newFile, fileDiffOpts);
     if (diff == undefined) {
-        console.log (`NO CHANGE FOR ${file}`);
+        console.log (`\nNO CHANGE FOR ${file}`);
         continue;
     }
 
-    console.log(`CHANGES IN ${file}:\n----------\n`)
-    console.log(jsonDiff.diffString(oldFile, newFile, {}, fileDiffOpts));
-    console.log('\n----------\n');
+    console.log(`\nCHANGES IN ${file}:`);
+
+    if (outputFullDiff) {
+        console.log(jsonDiff.diffString(oldFile, newFile, colorDiffOpts, fileDiffOpts).replace(/\u00a0/g, '\\u00a0'));
+    } else { // only show entries which changed
+        if (Array.isArray(diff)) {
+            // keywords file
+            logKeys('-', diffLinesWithCode(diff, '-').map(getKeywordName));
+            logKeys('+', diffLinesWithCode(diff, '+').map(getKeywordName))
+   
+            let changed = [];
+            let oldFileIndex = 0;
+            for (let diffIndex = 0; diffIndex < diff.length; diffIndex++) {
+                const code = diff[diffIndex][0];
+                if (code === '~') {
+                    changed.push(getKeywordName(oldFile[oldFileIndex]));
+                    oldFileIndex++;
+                } else if (code === '+') {
+                    continue;
+                } else {
+                    oldFileIndex++;
+                }
+            }
+            logKeys('~', changed);
+        } else { // object
+            const keys = Object.keys(diff);
+            const deleted = keys.filter(k => k.endsWith('__deleted')).map(k => k.replace('__deleted', ''));
+            logKeys('-', deleted);
+            const added = keys.filter(k => k.endsWith('__added')).map(k => k.replace('__added', ''));
+            logKeys('+', added);
+            const changed = keys.filter(k => !k.endsWith('__added') && !k.endsWith('__deleted'));
+            logKeys('~', changed);
+        }
+    }
 }
