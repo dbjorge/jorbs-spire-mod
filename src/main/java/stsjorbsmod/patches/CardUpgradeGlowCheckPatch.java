fd493156034a1ba7ee7d19a495d0232074cd4e7f
modargo <62083413+modargo@users.noreply.github.com>
2020-04-18 12:12:36 -0400

    Only check card glow in combat to avoid crash when upgrading cards outside of combat (#496)
    * Only check card glow in combat to avoid crash when upgrading cards outside of combat
    
    * Also check that AbstractDungeon.getCurrRoom() isn't null
    
    * Move each condition to its own line

--- src/main/java/stsjorbsmod/patches/CardUpgradeGlowCheckPatch.java
+++ src/main/java/stsjorbsmod/patches/CardUpgradeGlowCheckPatch.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 // This is for the benefit of the interaction between Warped Tongs and Mania's glow check
 public class CardUpgradeGlowCheckPatch {
@@ -16,7 +17,11 @@ public class CardUpgradeGlowCheckPatch {
         @SpirePostfixPatch
         public static void patch(AbstractCard __this)
         {
-            if (AbstractDungeon.player != null && AbstractDungeon.player.hand != null) {
+            if (AbstractDungeon.player != null &&
+                AbstractDungeon.player.hand != null &&
+                AbstractDungeon.getCurrRoom() != null &&
+                AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT)
+            {
                 AbstractDungeon.player.hand.glowCheck();
             }
         }
