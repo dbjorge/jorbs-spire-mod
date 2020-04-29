cf0c9e34f0d16f386b2f00be053d076d30a72ef4
Alex Courrau <acourrau@gmail.com>
2020-04-29 17:44:50 -0600

    First cut adding Demonic Coup card

---
+++ src/main/java/stsjorbsmod/cards/cull/deckoftrials/DemoicCoup.java
@@ -0,0 +1,50 @@
+package stsjorbsmod.cards.cull.deckoftrials;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.RitualPower;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.ConsumeCardAction;
+import stsjorbsmod.actions.DamageWithOnKillEffectAction;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+import stsjorbsmod.patches.EphemeralField;
+import stsjorbsmod.powers.FindWeaknessPower;
+
+import static stsjorbsmod.JorbsMod.JorbsCardTags.PERSISTENT_POSITIVE_EFFECT;
+
+public class DemoicCoup extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(DemoicCoup.class);
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int RITUAL_AMOUNT = 2;
+    private static final int UPGRADE_RITUAL_AMOUNT = 3;
+
+    public DemoicCoup() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+
+        magicNumber = baseMetaMagicNumber = RITUAL_AMOUNT;
+        EphemeralField.ephemeral.set(this, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new RitualPower(p, RITUAL_AMOUNT, true)));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_RITUAL_AMOUNT);
+            upgradeDescription();
+        }
+    }
+}
