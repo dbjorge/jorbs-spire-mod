ee8d7138f754cb05344ae081cbe21a4d64cafea2
Alex Courrau <acourrau@gmail.com>
2020-04-30 14:01:13 -0600

    First cut of Ticking Curse

---
+++ src/main/java/stsjorbsmod/cards/cull/deckoftrials/TickingCurse.java
@@ -0,0 +1,62 @@
+package stsjorbsmod.cards.cull.deckoftrials;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.ConsumeCardAction;
+import stsjorbsmod.actions.DamageWithOnKillEffectAction;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+import stsjorbsmod.patches.SelfExertField;
+
+import static stsjorbsmod.JorbsMod.JorbsCardTags.PERSISTENT_POSITIVE_EFFECT;
+
+public class TickingCurse extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(TickingCurse.class);
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 11;
+    private static final int UPGRADE_DAMAGE = 4;
+    private static final int EXHAUST_DAMAGE = 5;
+    private static final int UPGRADE_EXHAUST_DAMAGE = 3;
+
+    public TickingCurse() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = EXHAUST_DAMAGE;
+        this.isEthereal = true;
+        SelfExertField.selfExert.set(this, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAllEnemiesAction(p, DAMAGE, this.damageType, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+    public void triggerOnExhaust() {
+        if (this.upgraded) {
+            this.addToBot(new DamageAllEnemiesAction(null, EXHAUST_DAMAGE + UPGRADE_EXHAUST_DAMAGE, this.damageType, AbstractGameAction.AttackEffect.FIRE));
+        } else {
+            this.addToBot(new DamageAllEnemiesAction(null, EXHAUST_DAMAGE, this.damageType, AbstractGameAction.AttackEffect.FIRE));
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeMagicNumber(UPGRADE_EXHAUST_DAMAGE);
+            upgradeDescription();
+        }
+    }
+}
