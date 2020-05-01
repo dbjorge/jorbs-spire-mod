ee8a5ebe47ff2933ac7573b7164308e47ae17c20
nl364 <nlabuda03@gmail.com>
2020-05-01 19:41:31 -0400

    Added blasphemer class

---
+++ src/main/java/stsjorbsmod/cards/cull/deckoftrials/Blasphemer.java
@@ -0,0 +1,48 @@
+package stsjorbsmod.cards.cull.deckoftrials;
+
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.tempCards.Smite;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+
+public class Blasphemer extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(Blasphemer.class);
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int SMITE_AMOUNT = 5;
+    private static final int UPGRADE_SMITE_AMOUNT = 1;
+
+    public Blasphemer() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+        magicNumber = baseMagicNumber = SMITE_AMOUNT;
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        this.addToTop(new MakeTempCardInHandAction(new Smite(), this.magicNumber));
+        this.addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(UPGRADE_SMITE_AMOUNT);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+    }
+}
