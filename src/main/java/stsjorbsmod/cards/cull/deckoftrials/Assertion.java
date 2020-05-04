b4b68c68e622e8d029df30e4572e96db5909d6ae
Alex Courrau <acourrau@gmail.com>
2020-05-04 15:38:46 -0600

    Working Assertion Card, and updated "art"

--- src/main/java/stsjorbsmod/cards/cull/deckoftrials/Assertion.java
+++ src/main/java/stsjorbsmod/cards/cull/deckoftrials/Assertion.java
@@ -2,20 +2,15 @@ package stsjorbsmod.cards.cull.deckoftrials;
 
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.characters.Cull;
 import stsjorbsmod.characters.SkillsPlayedThisActSaveData;
 import stsjorbsmod.patches.SelfExertField;
-
-import java.util.ArrayList;
 
 import static stsjorbsmod.JorbsMod.MOD_ID;
 
@@ -28,7 +23,8 @@ public class Assertion extends CustomJorbsModCard {
     public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 3;
+    private static final int DAMAGE = 0;
+    private static final int DAMAGE_PER_SKILL = 3;
     private static final int MAX_DAMAGE = 50;
     private static final int UPGRADE_MAX_DAMAGE_AMOUNT = 25;
 
@@ -36,17 +32,17 @@ public class Assertion extends CustomJorbsModCard {
         super(ID, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
         magicNumber = baseMagicNumber = MAX_DAMAGE;
+        metaMagicNumber = baseMetaMagicNumber = DAMAGE_PER_SKILL;
         SelfExertField.selfExert.set(this, true);
+
+        this.rawDescription = cardStrings.DESCRIPTION;
+        this.rawDescription += metaMagicNumber + cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeDescription();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        SkillsPlayedThisActSaveData sp =
-                (SkillsPlayedThisActSaveData)BaseMod.getSaveFields().get(MOD_ID + ":SkillsPlayedThisAct");
-
-        for (int i = 0; i < sp.skillsPlayed; i++) {
-            addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        }
+        addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
     @Override
@@ -56,15 +52,23 @@ public class Assertion extends CustomJorbsModCard {
                 (SkillsPlayedThisActSaveData)BaseMod.getSaveFields().get(MOD_ID + ":SkillsPlayedThisAct");
 
         this.rawDescription = cardStrings.DESCRIPTION;
-        this.rawDescription += sp.skillsPlayed;
+        this.rawDescription += metaMagicNumber + cardStrings.EXTENDED_DESCRIPTION[0];
         if (sp.skillsPlayed == 1) {
-            this.rawDescription += cardStrings.EXTENDED_DESCRIPTION[0];
+            this.rawDescription += sp.skillsPlayed + cardStrings.EXTENDED_DESCRIPTION[1];
         } else {
-            this.rawDescription += cardStrings.EXTENDED_DESCRIPTION[1];
+            this.rawDescription += sp.skillsPlayed + cardStrings.EXTENDED_DESCRIPTION[2];
         }
-        this.rawDescription += cardStrings.EXTENDED_DESCRIPTION[2];
+        this.rawDescription += cardStrings.EXTENDED_DESCRIPTION[3];
 
         this.initializeDescription();
+    }
+
+    @Override
+    protected int calculateBonusBaseDamage() {
+        SkillsPlayedThisActSaveData sp =
+                (SkillsPlayedThisActSaveData)BaseMod.getSaveFields().get(MOD_ID + ":SkillsPlayedThisAct");
+
+        return Math.min(magicNumber, sp.skillsPlayed * metaMagicNumber);
     }
 
     @Override
