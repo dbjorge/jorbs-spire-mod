f4d1c12a91b496f897ba00858d74a86da75d0671
Alex Courrau <acourrau@gmail.com>
2020-05-08 12:39:29 -0600

    images - new power to store overkill - basic card def - localization strings

---
+++ src/main/java/stsjorbsmod/cards/cull/ReapAndSow.java
@@ -0,0 +1,59 @@
+package stsjorbsmod.cards.cull;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import stsjorbsmod.JorbsMod;
+import stsjorbsmod.actions.IncreaseManifestAction;
+import stsjorbsmod.cards.CustomJorbsModCard;
+import stsjorbsmod.characters.Cull;
+import stsjorbsmod.powers.OverkillPower;
+import stsjorbsmod.powers.ReapAndSowPower;
+
+public class ReapAndSow extends CustomJorbsModCard {
+    public static final String ID = JorbsMod.makeID(ReapAndSow.class);
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Cull.Enums.CULL_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 25;
+    private static final int UPGRADE_DAMAGE = 8;
+
+    private int overkill = 0;
+
+    public ReapAndSow() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = 0;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        overkill = getOverKill(m);
+        addToBot(new ApplyPowerAction(p, p, new ReapAndSowPower(p, overkill)));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+    private int getOverKill(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        if (this.damage > mo.currentHealth) {
+            this.overkill = this.damage - mo.currentHealth;
+        }
+        return overkill;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeDescription();
+        }
+    }
+}
---
+++ src/main/java/stsjorbsmod/powers/ReapAndSowPower.java
@@ -0,0 +1,39 @@
+package stsjorbsmod.powers;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+
+public class ReapAndSowPower extends CustomJorbsModPower {
+    public static final StaticPowerInfo STATIC = StaticPowerInfo.Load(ReapAndSowPower.class);
+    public static final String POWER_ID = STATIC.ID;
+
+    public ReapAndSowPower(AbstractCreature owner, int amount) {
+        super(STATIC);
+        this.owner = owner;
+        this.type = PowerType.BUFF;
+        this.amount = amount;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = String.format(DESCRIPTIONS[0], this.amount);
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new ReapAndSowPower(owner, amount);
+    }
+
+    @Override
+    public void onVictory() {
+        super.onVictory();
+        // Save value of this.amount;
+    }
+}
