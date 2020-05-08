fb5ae62227455d66f50c811882b7db34ee55e1dd
Alex Courrau <acourrau@gmail.com>
2020-05-08 15:05:48 -0600

    better save reap and sow data

--- src/main/java/stsjorbsmod/powers/ReapAndSowPower.java
+++ src/main/java/stsjorbsmod/powers/ReapAndSowPower.java
@@ -1,11 +1,6 @@
 package stsjorbsmod.powers;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import stsjorbsmod.characters.ReapAndSowSaveData;
 
@@ -18,7 +13,7 @@ public class ReapAndSowPower extends CustomJorbsModPower {
         super(STATIC);
         this.owner = owner;
         this.type = PowerType.BUFF;
-        this.amount = amount;
+        this.amount = ReapAndSowSaveData.reapAndSowDamage = amount;
         updateDescription();
     }
 
@@ -30,11 +25,5 @@ public class ReapAndSowPower extends CustomJorbsModPower {
     @Override
     public AbstractPower makeCopy() {
         return new ReapAndSowPower(owner, amount);
-    }
-
-    @Override
-    public void onVictory() {
-        super.onVictory();
-        ReapAndSowSaveData.reapAndSowDamage = this.amount;
     }
 }
