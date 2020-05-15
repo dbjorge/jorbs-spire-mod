b63ff6502beb14d4cf75ebae325974b0bc15b4c3
Alex Courrau <acourrau@gmail.com>
2020-05-15 09:25:57 -0600

    removing unused imports

--- src/main/java/stsjorbsmod/cards/cull/OldBook.java
+++ src/main/java/stsjorbsmod/cards/cull/OldBook.java
@@ -1,13 +1,7 @@
 package stsjorbsmod.cards.cull;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
