2ba5422ca05fa3e6fedee7adf040d2d30cef3323
Devin Owens <d.xcski@gmail.com>
2020-11-10 21:18:00 -0500

    Added Siphon Max HP change and infrastructure for it

--- src/main/java/stsjorbsmod/cards/cull/Siphon.java
+++ src/main/java/stsjorbsmod/cards/cull/Siphon.java
@@ -1,11 +1,11 @@
 package stsjorbsmod.cards.cull;
 
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.actions.DamageWithOnKillEffectAction;
+import stsjorbsmod.actions.IncreaseMaxHpByFlatAmountActionAndHealAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.characters.Cull;
 
@@ -22,18 +22,20 @@ public class Siphon extends CustomJorbsModCard {
     private static final int UPGRADE_DAMAGE = 3;
     private static final int HEAL_ON_FATAL = 4;
     private static final int UPGRADE_PLUS_HEAL_ON_FATAL = 1;
+    private static final int MAX_HP_ON_FATAL = 1;
 
 
     public Siphon() {
         super(ID, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
+        metaMagicNumber = baseMetaMagicNumber = MAX_HP_ON_FATAL;
         magicNumber = baseMagicNumber = HEAL_ON_FATAL;
         tags.add(CardTags.HEALING);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Runnable onKillEffect = () -> addToBot(new HealAction(p, p, magicNumber));
+        Runnable onKillEffect = () -> addToBot(new IncreaseMaxHpByFlatAmountActionAndHealAction(p, p, magicNumber, metaMagicNumber, true));
         this.addToBot(new DamageWithOnKillEffectAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), onKillEffect, true));
     }
 
