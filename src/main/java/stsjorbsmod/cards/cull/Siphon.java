54467551ff638f6dc8ec2ea81a937efbeb5dd20a
Devin Owens <d.xcski@gmail.com>
2020-11-12 17:10:20 -0500

    Removed The combined action since it was not needed

--- src/main/java/stsjorbsmod/cards/cull/Siphon.java
+++ src/main/java/stsjorbsmod/cards/cull/Siphon.java
@@ -1,11 +1,12 @@
 package stsjorbsmod.cards.cull;
 
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import stsjorbsmod.JorbsMod;
 import stsjorbsmod.actions.DamageWithOnKillEffectAction;
-import stsjorbsmod.actions.IncreaseMaxHpByFlatAmountActionAndHealAction;
+import stsjorbsmod.actions.IncreaseMaxHpByFlatAmountAction;
 import stsjorbsmod.cards.CustomJorbsModCard;
 import stsjorbsmod.characters.Cull;
 
@@ -34,8 +35,10 @@ public class Siphon extends CustomJorbsModCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Runnable onKillEffect = () -> addToBot(
-                new IncreaseMaxHpByFlatAmountActionAndHealAction(p, p, magicNumber, metaMagicNumber, true));
+        Runnable onKillEffect = () -> {
+            addToBot(new HealAction(p, p, magicNumber));
+            addToBot(new IncreaseMaxHpByFlatAmountAction(p, p, metaMagicNumber, true));
+        };
         this.addToBot(new DamageWithOnKillEffectAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
                 onKillEffect, true));
     }
